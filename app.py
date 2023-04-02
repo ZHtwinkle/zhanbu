1eaed3b71d43bb49fddd48b0837685d2dedae1dc
ZHtwinkle <79469234+ZHtwinkle@users.noreply.github.com>
2023-04-03 01:47:59 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import openai
+import os
+
+app = Flask(__name__)
+
+# 加载环境变量，获取OpenAI API密钥
+openai.api_key = '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'
+
+# 设置占卜的模型ID和引擎参数
+model_id = "tarot"
+engine = "davinci"
+
+# 渲染主页
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 渲染占卜页面
+@app.route('/fortune-telling')
+def fortune_telling():
+    return render_template('fortune_telling.html')
+
+# 处理占卜请求
+@app.route('/fortune-telling', methods=['POST'])
+def fortune_telling_post():
+    zodiac = request.form['zodiac']
+    age = request.form['age']
+    question = request.form['question']
+
+    # 根据用户提出的问题，调用OpenAI API获取回答
+    prompt = f"{zodiac}座，{age}岁，问：{question}"
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # 从API的回答结果中提取生成的文本并返回给用户
+    answer = response.choices[0].text.strip()
+    return render_template('fortune_telling.html', answer=answer)
+
+if __name__ == '__main__':
+    app.run()
