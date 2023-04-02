50f22340cf25e0b43b6cb4213eaa95fe0fb66cd6
ZHtwinkle <79469234+ZHtwinkle@users.noreply.github.com>
2023-04-03 01:46:20 +0800

    Add files via upload

---
+++ 占卜网站/chat.py
@@ -0,0 +1,25 @@
+import openai
+
+# 加载环境变量，获取OpenAI API密钥
+openai.api_key = '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'
+
+
+# 提问代码
+def chat_gpt(prompt):
+    # 你的问题
+    prompt = prompt
+    
+    # 调用 ChatGPT 接口
+    model_engine = "text-davinci-003"
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    response = completion.choices[0].text
+    print(response)
+chat_gpt("我的爱情多会来？")
